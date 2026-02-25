b04cd021eef4bd9547786b5b594858af8962ec8d
G0wreesan <rajaheeesan@gmail.com>
2026-02-25 21:07:04 +0530

    getting to know about methods and functions differentations

---
+++ day_10/lists.py
@@ -0,0 +1,41 @@
+"""spam = ['cat', 'dog', 'moose']
+for i in spam:
+    print(i)
+    
+    this prints each element in the list spam on a new line
+"""
+
+"""
+spam = ['cat', 'dog', 'moose']
+for i in range(len(spam)):
+    print(i)
+this here prints the index of each element like 0 , 1 , 2 
+    because range(len(spam)) generates a sequence of numbers from 0 to the length of the list minus one,
+    which corresponds to the indices of the list elements.
+    
+"""
+
+"""spam = ['cat', 'dog', 'moose']
+for i in range(len(spam)):
+    print(spam[i])
+Because we use the Print with spame[i] which prints the elements cuz i goes from 0-2 in range(len(spam))
+"""
+""""
+spam = 100
+for i in range(5):
+    
+    spam += 1
+print(spam)
+ this will print 105
+"""
+"""
+21. spam = spam * 2 || spam*=2
+
+22. bacon = bacon - 3 || bacon -=3
+
+23. eggs = eggs + bacon * 5 || eggs += bacon * 5
+
+24. eggs = eggs * bacon + 5 || eggs *= bacon + 5
+
+25. spam = spam + 'LastName' || spam += 'LastName'
+"""
