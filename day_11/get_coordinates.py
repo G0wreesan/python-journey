3b416fbe246494f78bdfb40af1659791ec843804
G0wreesan <rajaheeesan@gmail.com>
2026-03-02 23:00:07 +0530

    get coordinates script with testing input

--- day_11/get_coordinates.py
+++ day_11/get_coordinates.py
@@ -1,2 +1,21 @@
-def get_Cordinates(texts):
-    
+
+directions =[];
+
+def get_coordinates(directions):
+    x=0;
+    y=0;
+    for direction in directions:
+        if direction == "N":
+            y += 1;
+        elif direction == "S":
+            y -= 1;
+        elif direction == "E":
+            x += 1;
+        elif direction == "W":
+            x -= 1;
+    return (x, y);
+
+# example 
+directions = ["N", "E", "S", "W", "N", "E"];
+coordinates = get_coordinates(directions);
+print(coordinates);  # Output: (1, 1)
