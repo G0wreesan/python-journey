54416966fb91bda66ed726401180d6c27c607048
G0wreesan <rajaheeesan@gmail.com>
2026-03-02 23:16:27 +0530

    working get_coordinates python script

--- day_11/get_coordinates.py
+++ day_11/get_coordinates.py
@@ -19,3 +19,16 @@ def get_coordinates(directions):
 directions = ["N", "E", "S", "W", "N", "E"];
 coordinates = get_coordinates(directions);
 print(coordinates);  # Output: (1, 1)
+
+print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+while True:
+    user_input=input("enter directions (N,E,W,S) one by one blank enter show the results : ").strip().upper();
+    if user_input == "":
+        print("calculating final coordinates...");
+        break;
+    if user_input in ["N", "E", "W", "S"]:
+        directions.append(user_input);
+    else:
+        print("invalid input only N,E,W,S are allowed");
+final_coordinates = get_coordinates(directions);
+print(f"final coordinates: {final_coordinates}");
