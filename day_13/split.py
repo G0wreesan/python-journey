cf65774264f17b7f7f414505b1ee4114fad8499b
G0wreesan <rajaheeesan@gmail.com>
2026-05-07 22:47:12 +0530

    splitting words function updated

--- day_13/split.py
+++ day_13/split.py
@@ -1,4 +1,4 @@
-import sys
+import sys,time
 
 def split_sentence(sentence):
     words = sentence.split()
@@ -7,4 +7,12 @@ def split_sentence(sentence):
 sentence = input("Enter a sentence: ")
 words = split_sentence(sentence)
 for i in range(len(words)):
+    if i==0:
+        print("Splitting the sentence into words...")
+        time.sleep(1)
+    elif i==len(words):
+        print("\n All words have been displayed.")
+        time.sleep(1)
+    
     print(f"Word {i + 1}: {words[i]}")
+    time.sleep(0.5)
